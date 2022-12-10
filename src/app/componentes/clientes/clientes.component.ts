cb9a793e5eff73190eba553ad127c884b98a2a74
jhonnyer <jhonnyerg@unicauca.edu.co>
2022-12-09 20:01:15 -0500

    Interpolacion 1

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -13,19 +13,19 @@ export class ClientesComponent {
   constructor(){
     this.cliente1=new Cliente(1,'Jhonnyer','Galindez', 'jhonnyer@gmail.com','2022-12-09','');
     // this.cliente1={id: 1, nombre: 'Luis', apellido: 'Burbano', email:'luis@gmail.com',createAt:'2022-12-09',foto:''};
-    this.clientes=[];
-    // this.clientes=[
-    //   {id: 1, nombre: 'Luis', apellido: 'Burbano', email:'luis@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 2, nombre: 'Alexandra', apellido: 'Zalazar', email:'alexandra@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 3, nombre: 'Fernando', apellido: 'Melendez', email:'fernando@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 4, nombre: 'Ana', apellido: 'Erazo', email:'ana@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 5, nombre: 'Eduardo', apellido: 'Bermudez', email:'eduardo@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 6, nombre: 'Jesus', apellido: 'Zambrano', email:'jesus@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 7, nombre: 'Daniel', apellido: 'Burbano', email:'daniel@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 8, nombre: 'Angelica', apellido: 'Burbano', email:'angelica@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 9, nombre: 'Luisa', apellido: 'Burbano', email:'luisa@gmail.com',createAt:'2022-12-09',foto:''},
-    //   {id: 10, nombre: 'Sebastian', apellido: 'Burbano', email:'sebastian@gmail.com',createAt:'2022-12-09',foto:''},
-    // ]
+    // this.clientes=[];
+    this.clientes=[
+      {id: 1, nombre: 'Luis', apellido: 'Burbano', email:'luis@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 2, nombre: 'Alexandra', apellido: 'Zalazar', email:'alexandra@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 3, nombre: 'Fernando', apellido: 'Melendez', email:'fernando@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 4, nombre: 'Ana', apellido: 'Erazo', email:'ana@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 5, nombre: 'Eduardo', apellido: 'Bermudez', email:'eduardo@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 6, nombre: 'Jesus', apellido: 'Zambrano', email:'jesus@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 7, nombre: 'Daniel', apellido: 'Burbano', email:'daniel@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 8, nombre: 'Angelica', apellido: 'Burbano', email:'angelica@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 9, nombre: 'Luisa', apellido: 'Burbano', email:'luisa@gmail.com',createAt:'2022-12-09',foto:''},
+      {id: 10, nombre: 'Sebastian', apellido: 'Burbano', email:'sebastian@gmail.com',createAt:'2022-12-09',foto:''},
+    ]
   }
 
 }
